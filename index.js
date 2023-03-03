9f91f7474fb54ee8bec63425e6cf4e3a38d1115a
Chhatrapati Mandwe <mandwec12@gmail.com>
2023-03-03 20:54:43 +0530

    calci update

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
-let string = ""
-
-let display = document.querySelector(".display")
-
-let btns = document.querySelectorAll(".btn")
+let string = "";
+let display = document.querySelector(".display");
+let btns = document.querySelectorAll(".btn");
 Array.from(btns).forEach((button) => {
-    button.addEventListener("click", (e) => {
-        console.log(e.target.innerHTML)
-        if (e.target.innerHTML == "C") {
-            string = ""
-            display.innerHTML = string
-        } else if (e.target.innerHTML == "=") {
-            if (string == "") {
-                return
-            }
-            string = eval(string)
-            display.innerHTML = string
-        } else if (e.target.innerHTML == "Back") {
-            string = string.toString()
-            string = string.slice(0, string.length - 1)
-            display.innerHTML = string
-        } else {
-            if ((string[string.length - 1] == "-" ||string[string.length - 1] == "+" || string[string.length - 1] == "*" || string[string.length - 1] == "." || string[string.length - 1] == "/") && (e.target.innerHTML == "-" || e.target.innerHTML == "+" || e.target.innerHTML == "*" || e.target.innerHTML == "." || e.target.innerHTML == "/")) {
-                return
-            }
-            if (string == "" && (e.target.innerHTML == "/" || e.target.innerHTML == "." || e.target.innerHTML == "*")) {
-                display.innerHTML = string
-                return
-            }
-            string = string + e.target.innerHTML
-            display.innerHTML = string
-
-        }
-    })
-})
+  button.addEventListener("click", (e) => {
+    console.log(e.target.innerHTML);
+    if (e.target.innerHTML == "C") {
+      string = "";
+      display.innerHTML = string;
+    } else if (e.target.innerHTML == "=") {
+      if (string == "") {
+        return;
+      }
+      string = eval(string);
+      display.innerHTML = string;
+    } else if (e.target.innerHTML == "Back") {
+      string = string.toString();
+      string = string.slice(0, string.length - 1);
+      display.innerHTML = string;
+    } else {
+      if (
+        (string[string.length - 1] == "-" ||
+          string[string.length - 1] == "+" ||
+          string[string.length - 1] == "*" ||
+          string[string.length - 1] == "." ||
+          string[string.length - 1] == "/") &&
+        (e.target.innerHTML == "-" ||
+          e.target.innerHTML == "+" ||
+          e.target.innerHTML == "*" ||
+          e.target.innerHTML == "." ||
+          e.target.innerHTML == "/")
+      ) {
+        return;
+      }
+      if (
+        string == "" &&
+        (e.target.innerHTML == "/" ||
+          e.target.innerHTML == "." ||
+          e.target.innerHTML == "*")
+      ) {
+        display.innerHTML = string;
+        return;
+      }
+      string = string + e.target.innerHTML;
+      display.innerHTML = string;
+    }
+  });
+});
