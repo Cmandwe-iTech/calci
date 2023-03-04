3575cd7c22e7c9dde9154d5c78e9f99ed2a07a3d
Chhatrapati Mandwe <mandwec12@gmail.com>
2023-03-04 13:12:24 +0530

    add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let string = "";
 let display = document.querySelector(".display");
 let btns = document.querySelectorAll(".btn");
+let toggle = true;
 Array.from(btns).forEach((button) => {
   button.addEventListener("click", (e) => {
     console.log(e.target.innerHTML);
@@ -12,37 +13,105 @@ Array.from(btns).forEach((button) => {
         return;
       }
       string = eval(string);
+      console.log(string, typeof(string));
       display.innerHTML = string;
+      if(string.toString().includes(".")){
+        toggle = false
+        console.log("hiii");
+      }else{
+        toggle = true;
+      }
     } else if (e.target.innerHTML == "Back") {
       string = string.toString();
       string = string.slice(0, string.length - 1);
       display.innerHTML = string;
     } else {
-      if (
-        (string[string.length - 1] == "-" ||
-          string[string.length - 1] == "+" ||
-          string[string.length - 1] == "*" ||
-          string[string.length - 1] == "." ||
-          string[string.length - 1] == "/") &&
-        (e.target.innerHTML == "-" ||
-          e.target.innerHTML == "+" ||
-          e.target.innerHTML == "*" ||
-          e.target.innerHTML == "." ||
-          e.target.innerHTML == "/")
-      ) {
-        return;
-      }
-      if (
-        string == "" &&
-        (e.target.innerHTML == "/" ||
-          e.target.innerHTML == "." ||
-          e.target.innerHTML == "*")
-      ) {
+      if(e.target.innerHTML === "."){
+         if(toggle === true){
+          if (
+            (string[string.length - 1] == "-" ||
+              string[string.length - 1] == "+" ||
+              string[string.length - 1] == "*" ||
+              string[string.length - 1] == "." ||
+              string[string.length - 1] == "/") &&
+            (e.target.innerHTML == "-" ||
+              e.target.innerHTML == "+" ||
+              e.target.innerHTML == "*" ||
+              e.target.innerHTML == "." ||
+              e.target.innerHTML == "/")
+          ) {
+            return;
+          }
+          if (
+            string == "" &&
+            (e.target.innerHTML == "/" ||
+              e.target.innerHTML == "." ||
+              e.target.innerHTML == "*")
+          ) {
+            display.innerHTML = string;
+            return;
+          }
+          string = string + e.target.innerHTML;
+          display.innerHTML = string;
+          toggle = false
+         } else{
+          return
+         }
+      }else if(["+","-","*","/"].includes(e.target.innerHTML)){
+        if (
+          (string[string.length - 1] == "-" ||
+            string[string.length - 1] == "+" ||
+            string[string.length - 1] == "*" ||
+            string[string.length - 1] == "." ||
+            string[string.length - 1] == "/") &&
+          (e.target.innerHTML == "-" ||
+            e.target.innerHTML == "+" ||
+            e.target.innerHTML == "*" ||
+            e.target.innerHTML == "." ||
+            e.target.innerHTML == "/")
+        ) {
+          return;
+        }
+        if (
+          string == "" &&
+          (e.target.innerHTML == "/" ||
+            e.target.innerHTML == "." ||
+            e.target.innerHTML == "*")
+        ) {
+          display.innerHTML = string;
+          console.log("hello",string);
+          return;
+        }
+        string = string + e.target.innerHTML;
         display.innerHTML = string;
-        return;
+        toggle = true;
+      }else{
+        if (
+          (string[string.length - 1] == "-" ||
+            string[string.length - 1] == "+" ||
+            string[string.length - 1] == "*" ||
+            string[string.length - 1] == "." ||
+            string[string.length - 1] == "/") &&
+          (e.target.innerHTML == "-" ||
+            e.target.innerHTML == "+" ||
+            e.target.innerHTML == "*" ||
+            e.target.innerHTML == "." ||
+            e.target.innerHTML == "/")
+        ) {
+          return;
+        }
+        if (
+          string == "" &&
+          (e.target.innerHTML == "/" ||
+            e.target.innerHTML == "." ||
+            e.target.innerHTML == "*")
+        ) {
+          display.innerHTML = string;
+          return;
+        }
+        string = string + e.target.innerHTML;
+        display.innerHTML = string;
       }
-      string = string + e.target.innerHTML;
-      display.innerHTML = string;
     }
   });
 });
